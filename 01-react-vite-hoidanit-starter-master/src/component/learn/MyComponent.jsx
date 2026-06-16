ceeb49659d8b15455d26430e1f1420244653eaf3
Lan Anh <nguyenlananh2906@gmail.com>
2026-06-16 16:14:43 +0700

    buoi 31

--- 01-react-vite-hoidanit-starter-master/src/component/learn/MyComponent.jsx
+++ 01-react-vite-hoidanit-starter-master/src/component/learn/MyComponent.jsx
@@ -1,18 +1,22 @@
 //JSX = JavaScript XML
 import './style.css'
-const FakeComponent = () => {
-    return (
-        <div>
-            fake component
-        </div>
-    )
+//const name = "lan anh 1"
+//const name = "25"
+//const name = true
+//const name = undefined
+//const name = null
+
+//const name = [1, 2, 3]
+const name = {
+    name: "lan anh",
+    age: 25
 }
 //fragment = <> </>  // khong can them div bao quanh
 const MyComponent = () => {
 
     return (
         <>
-            <div> eric & lan anh update </div>
+            <div> eric & {JSON.stringify(name)} update </div>
             <div className="my-class" style={{ borderRadius: '10px' }}> class </div>
         </>
     );
