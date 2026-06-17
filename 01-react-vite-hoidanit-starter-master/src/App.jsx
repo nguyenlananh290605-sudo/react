0a83a0fe3604c56ea0bc014ffd88a159745399d9
Lan Anh <nguyenlananh2906@gmail.com>
2026-06-17 17:28:32 +0700

    video 47

--- 01-react-vite-hoidanit-starter-master/src/App.jsx
+++ 01-react-vite-hoidanit-starter-master/src/App.jsx
@@ -3,6 +3,8 @@ import TodoNew from './components/todo/TodoNew'
 import TodoData from './components/todo/TodoData'
 import reactLogo from './assets/react.svg'
 import { useState } from 'react'
+import Header from './layout/header'
+import Footer from './layout/footer'
 
 const App = () => {
   const [todoList, setTodoList] = useState([
@@ -30,23 +32,28 @@ const App = () => {
 
 
   return (
-    <div className='todo-container'>
-      <div className='todo-title'>Todo List</div>
-      <TodoNew
-        addNewTodo={addNewTodo}
-      />
-      {todoList.length > 0 ?
-        <TodoData
-          todoList={todoList}
-          deleteTodo={deleteTodo}
+    <>
+      <Header />
+      <div className='todo-container'>
+        <div className='todo-title'>Todo List</div>
+        <TodoNew
+          addNewTodo={addNewTodo}
         />
-        :
+        {todoList.length > 0 ?
+          <TodoData
+            todoList={todoList}
+            deleteTodo={deleteTodo}
+          />
+          :
 
-        <div className='todo-image'>
-          <img src={reactLogo} alt="Todo Image" />
-        </div>
-      }
-    </div>
+          <div className='todo-image'>
+            <img src={reactLogo} alt="Todo Image" />
+          </div>
+        }
+      </div>
+      <Footer />
+    </>
+
   )
 }
 
