cc3e6d77be98794fdb2bb065472ce7252ceb841d
Lan Anh <nguyenlananh2906@gmail.com>
2026-06-17 14:31:25 +0700

    buoi 36

--- 01-react-vite-hoidanit-starter-master/src/component/todo/TodoNew.jsx
+++ 01-react-vite-hoidanit-starter-master/src/component/todo/TodoNew.jsx
@@ -2,10 +2,21 @@ const TodoNew = (props) => {
     console.log(">>check point: ", props);
     const { addNewTodo } = props; //destructuring
     // addNewTodo("lan anh"); //goi ham addNewTodo tu App truyen vao
+
+    const handleClick = () => {
+        alert(">>click me");
+    }
+    const handleOnChange = (name) => {
+        console.log(">>handle onChange: ", name);
+    }
     return (
         <div className='todo-new'>
-            <input type="text" />
-            <button> Add </button>
+            <input type="text"
+                onChange={(event) => handleOnChange(event.target.value)}
+            />
+            <button style={{ cursor: 'pointer' }}
+                onClick={handleClick}
+            > Add </button>
         </div>
     )
 }
