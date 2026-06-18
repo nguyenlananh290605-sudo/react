bf4b796563e89be0dbbf70cf073011e8e3e72bef
Lan Anh <nguyenlananh2906@gmail.com>
2026-06-18 09:36:31 +0700

    buoi 52

--- 01-react-vite-hoidanit-starter-master/src/main.jsx
+++ 01-react-vite-hoidanit-starter-master/src/main.jsx
@@ -8,7 +8,7 @@ import {
 import LoginPage from './pages/login.jsx';
 import RegisterPage from './pages/register.jsx';
 import UserPage from './pages/user.jsx';
-import ProductPage from './pages/product.jsx';
+import BookPage from './pages/book.jsx';
 import "./style/global.css"
 import TodoApp from './components/todo/TodoApp.jsx';
 import ErrorPage from './pages/error.jsx';
@@ -28,8 +28,8 @@ const router = createBrowserRouter([
         element: <UserPage />
       },
       {
-        path: "/products",
-        element: <ProductPage />
+        path: "/books",
+        element: <BookPage />
       }
     ]
   },
