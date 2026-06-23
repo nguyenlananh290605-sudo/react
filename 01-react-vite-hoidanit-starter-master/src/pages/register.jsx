844d8132af247a0db4d65a83912eba675c8d9dfe
Lan Anh <nguyenlananh2906@gmail.com>
2026-06-23 09:52:36 +0700

    video 89

--- 01-react-vite-hoidanit-starter-master/src/pages/register.jsx
+++ 01-react-vite-hoidanit-starter-master/src/pages/register.jsx
@@ -1,6 +1,61 @@
+import { Button, Input, Form } from "antd";
+
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+
+    const onFinish = (values) => {
+        console.log(">>> check values", values)
+    }
     return (
-        <div> register page</div>
+        <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+        //onFinishFailed={onFinishFailed}
+        >
+
+            <div style={{ margin: "50px" }}>
+
+                <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                //rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Email"
+                    name="email"
+                //rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                //rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="Phone"
+                    name="phoneNumber"
+                //rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <div>
+                    <Button
+                        onClick={() => form.submit()}
+                        type="primary" >
+                        Register
+                    </Button>
+                </div>
+            </div>
+        </Form>
+
     )
 }
 export default RegisterPage;
