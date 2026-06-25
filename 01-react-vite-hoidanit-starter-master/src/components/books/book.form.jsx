4209c3a6a8d1593d81cd61fc5061125bd15d44dc
Lan Anh <nguyenlananh2906@gmail.com>
2026-06-25 10:10:11 +0700

    108

--- 01-react-vite-hoidanit-starter-master/src/components/books/book.form.jsx
+++ 01-react-vite-hoidanit-starter-master/src/components/books/book.form.jsx
@@ -1,64 +1,208 @@
-import { Table, Button, Modal, notification } from 'antd';
+import { Table, Button, Modal, notification, Input, Select, InputNumber } from 'antd';
 import { useState } from 'react';
+import { createBookAPI, handleUploadFile } from '../../services/api.service';
 
 const BookForm = (props) => {
-    const { loadUser } = props
-    const [fullName, setFullName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [phone, setPhone] = useState("");
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const { loadBook } = props
+    const [mainText, setMainText] = useState("");
+    const [author, setAuthor] = useState("");
+    const [price, setPrice] = useState(0);
+    const [quantity, setQuantity] = useState(0);
+    const [category, setCategory] = useState("");
 
-    // const handleSubmitBtn = async () => {
-    //     const res = await createUserAPI(fullName, email, password, phone)
-    //     if (res.data) {
-    //         notification.success({
-    //             message: "Create user",
-    //             description: "Tạo user thành công"
-    //         })
-    //         resetAndCloseModal();
-    //         await loadUser();
-    //     }
-    //     else {
-    //         notification.error({
-    //             message: "Error create user",
-    //             description: JSON.stringify(res.message)
-    //         })
-    //     }
-    // }
-    const resetAndCloseModal = () => {
-        setIsModalOpen(false);
-        setFullName("");
-        setEmail("");
-        setPassword("");
-        setPhone("");
+    const [isCreateOpen, setIsCreateOpen] = useState(false);
+
+
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [preview, setPreview] = useState(null)
+
+    const handleOnChangeFile = (event) => {
+        if (!event.target.files || event.target.files.length === 0) {
+            setSelectedFile(null)
+            setPreview(null)
+            return;
+        }
+
+        // I've kept this example simple by using the first image instead of multiple
+        const file = event.target.files[0]
+        if (file) {
+            setSelectedFile(file)
+            setPreview(URL.createObjectURL(file))
+        }
+    }
+    const handleSubmitBtn = async () => {
+        if (!selectedFile) {
+            notification.error({
+                message: "Error create book",
+                description: "Vui long upload anh thumbnail"
+            })
+            return;
+        }
+
+        //upload file
+        const resUpload = await handleUploadFile(selectedFile, "book")
+        if (resUpload.data) {
+            //success
+            const newThumbnail = resUpload.data.fileUploaded;
+            const resBook = await createBookAPI(newThumbnail,
+                mainText, author, price, quantity, category
+            )
+            if (resBook.data) {
+                resetAndCloseCreate();
+                await loadBook();
+                notification.success({
+                    message: "Create book",
+                    description: "Tao book moi thanh cong"
+                })
+            }
+            else {
+                notification.error({
+                    message: "Error update avatar",
+                    description: JSON.stringify(resBook.message)
+                })
+            }
+        } else {
+            //failed
+            notification.error({
+                message: "Error upload file",
+                description: JSON.stringify(resUpload.message)
+            })
+        }
+
+
+    }
+    const resetAndCloseCreate = () => {
+        setIsCreateOpen(false);
+        setMainText("");
+        setAuthor("");
+        setPrice(0);
+        setQuantity(0);
+        setCategory("");
+        setSelectedFile(null);
+        setPreview(null);
+        setIsCreateOpen(false);
     }
 
     return (
         <div className="user-form" style={{ margin: "10px 0" }}>
             <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
                 <div style={{ display: "flex", justifyContent: "space-between" }}>
-                    <h3>Table Users</h3>
+                    <h3>Table Books</h3>
                     <Button
+                        onClick={() => setIsCreateOpen(true)}
                         type="primary">
-                        Create user
+                        Create book
                     </Button>
 
                 </div>
+                <Modal
+                    title="Create Book"
+                    closable={{ 'aria-label': 'Custom Close Button' }}
+                    open={isCreateOpen}
+                    onOk={() => handleSubmitBtn()}
+                    onCancel={() => resetAndCloseCreate()}
+                    maskClosable={false}
+                    okText={"Create"}
+                >
 
-            </div>
-            <Modal
-                title="Basic Modal"
-                closable={{ 'aria-label': 'Custom Close Button' }}
-                open={isModalOpen}
-                //onOk={handleOk}
-                onCancel={resetAndCloseModal}
-            >
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-            </Modal>
-        </div>
+                    <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
+                        <div>
+                            <span>MainText</span>
+                            <Input
+                                value={mainText}
+                                onChange={(event) => { setMainText(event.target.value) }}
+                            />
+                        </div>
+                        <div>
+                            <span>Author</span>
+                            <Input
+                                value={author}
+                                onChange={(event) => { setAuthor(event.target.value) }}
+                            />
+                        </div>
+                        <div >
+                            <span>Price</span>
+                            <InputNumber
+                                suffix="đ"
+                                style={{ width: '100%' }}
+                                min={1} max={5} defaultValue={3}
+                                onChange={(event) => { setPrice(event) }}
+                            />
+                        </div>
+                        <div >
+                            <span>Quantity</span>
+                            <InputNumber
+                                style={{ width: '100%' }}
+                                min={1} defaultValue={3}
+                                onChange={(event) => { setQuantity(event) }}
+                            />
+                        </div>
+                        <div  >
+                            <span>Category</span>
+                            <Select
+                                defaultValue="Business"
+                                style={{ width: '100%' }}
+                                onChange={(event) => { setCategory(event) }}
+                                options={[
+                                    { value: 'Arts', label: 'Arts' },
+                                    { value: 'Business', label: 'Business' },
+                                    { value: 'Comics', label: 'Comics' },
+
+                                    { value: 'Cooking', label: 'Cooking' },
+                                    { value: 'Entertainment', label: 'Entertainment' },
+                                    { value: 'History', label: 'History' },
+
+                                    { value: 'Music', label: 'Music' },
+                                    { value: 'Sports', label: 'Sports' },
+                                    { value: 'Teen', label: 'Teen' },
+                                    { value: 'Travel', label: 'Travel' },
+
+                                ]}
+
+                            />
+                        </div>
+                        <div>
+                            <span>Thumbnail</span>
+                            <div>
+                                <label htmlFor='btnUpload' style={{
+                                    display: "block",
+                                    width: "fit-content",
+                                    marginTop: "15px",
+                                    padding: "5px 10px",
+                                    background: "green",
+                                    borderRadius: "5px",
+                                    cursor: "pointer"
+
+                                }}>
+                                    Upload
+                                </label>
+                                <input
+                                    type='file' hidden id='btnUpload'
+                                    //onChange={handleOnChangeFile}
+                                    onChange={(event => handleOnChangeFile(event))}
+                                    onClick={(event) => event.target.value = null}
+                                ></input>
+                            </div>
+                            {preview &&
+                                <>
+                                    <div style={{
+                                        marginTop: "10px",
+                                        marginBottom: "15px",
+                                        height: "100px",
+                                        width: "150px",
+
+                                    }}>
+                                        <img style={{ height: "100%", width: "100%", objectFit: "contain" }}
+                                            src={preview} />
+                                    </div>
+
+                                </>
+                            }
+                        </div>
+                    </div>
+                </Modal>
+            </div >
+        </div >
     )
 }
 export default BookForm;
--- 01-react-vite-hoidanit-starter-master/src/services/api.service.js
+++ 01-react-vite-hoidanit-starter-master/src/services/api.service.js
@@ -85,22 +85,23 @@ const fetchAllBookAPI = (current, pageSize) => {
     const URL_BACKEND = `/api/v1/book?current=${current}&pageSize=${pageSize}`
     return axios.get(URL_BACKEND)
 }
-// const createBookAPI = (fullName, email, password, phone) => {
-//     const URL_BACKEND = "/api/v1/user"
-//     const data = {
-//         fullName: fullName,
-//         email: email,
-//         password: password,
-//         phone: phone
-//     }
-//     return axios.post(URL_BACKEND, data)
-// }
+const createBookAPI = (thumbnail, mainText, author, price, quantity, category) => {
+    const URL_BACKEND = "/api/v1/book"
+    const data = {
+        thumbnail: thumbnail,
+        mainText: mainText,
+        author: author,
+        price: price,
+        quantity: quantity,
+        category: category
+    }
+    return axios.post(URL_BACKEND, data)
+}
 export {
     createUserAPI, updateUserAPI,
     fetchAllUserAPI, deleteUserAPI,
     handleUploadFile, updateUserAvatarAPI,
     registerUserAPI, loginAPI,
     getAccountAPI, logoutAPI,
-
-    fetchAllBookAPI,
+    fetchAllBookAPI, createBookAPI
 }
